e117ad6dac43a36665be239555e471a26b384db2
GrypheNDravize <ratoge1816@gmail.com>
2025-07-07 20:32:44 +0800

    add: comment on BROADCAST_ADDR val, "not final"

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 PORT = 50999
-BROADCAST_ADDR = "255.255.255.255" # tells the operating system to send the packet to all devices on the current local network 
+BROADCAST_ADDR = "255.255.255.255" # tells the operating system to send the packet to all devices on the current local network
+								   # not final
 VERBOSE = True
