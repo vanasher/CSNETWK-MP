be89cb25f95a48b94a236ed7758d0ab5d80e4a11
GrypheNDravize <ratoge1816@gmail.com>
2025-07-15 11:25:25 +0800

    add: AVATAR_* fields for PROFILE message

--- core/peer.py
+++ core/peer.py
@@ -1,33 +1,38 @@
 # keeps track of all known peers and their data in a dictionary
 class PeerManager:
-    def __init__(self, logger):
-        self.logger = logger
-        self.peers = {} # stores disctionary of peers by USER_ID
-        				# user_id -> {display_name, status, posts: [], dms: []}
-                        
+	def __init__(self, logger):
+		self.logger = logger
+		self.peers = {} # stores disctionary of peers by USER_ID
+						# user_id -> {display_name, status, posts: [], dms: []}
+						
 	# add or update a peer's profile info
-    def add_peer(self, user_id, display_name, status):
-        if user_id not in self.peers:
-            self.peers[user_id] = {
-                'display_name': display_name,
-                'status': status,
-                'posts': [],
-                'dms': []
-            }
-        else:
-            self.peers[user_id]['display_name'] = display_name
-            self.peers[user_id]['status'] = status
+	def add_peer(self, user_id, display_name, status, avatar_type=None, avatar_data=None):
+		if user_id not in self.peers:
+			self.peers[user_id] = {
+				'display_name': display_name,
+				'status': status,
+				'avatar_type': avatar_type,
+				'avatar_data': avatar_data,
+				'posts': [],
+				'dms': []
+			}
+		else:
+			self.peers[user_id]['display_name'] = display_name
+			self.peers[user_id]['status'] = status
+			if avatar_type and avatar_data:
+				self.peers[user_id]['avatar_type'] = avatar_type
+				self.peers[user_id]['avatar_data'] = avatar_data
 
 	# add a new post to the peer's post list
-    def add_post(self, user_id, content):
-        if user_id in self.peers:
-            self.peers[user_id]['posts'].append(content)
+	def add_post(self, user_id, content):
+		if user_id in self.peers:
+			self.peers[user_id]['posts'].append(content)
 
 	# add a new dm to the peer's dm list
-    def add_dm(self, user_id, content):
-        if user_id in self.peers:
-            self.peers[user_id]['dms'].append(content)
+	def add_dm(self, user_id, content):
+		if user_id in self.peers:
+			self.peers[user_id]['dms'].append(content)
 
 	# returns a list of (user_id, display_name) for all known peers
-    def list_peers(self):
-        return [(uid, info['display_name']) for uid, info in self.peers.items()]
+	def list_peers(self):
+		return [(uid, info['display_name']) for uid, info in self.peers.items()]
