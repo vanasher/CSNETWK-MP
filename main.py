4e369c203bc283e2a7746634eb66473c06324d47
GrypheNDravize <ratoge1816@gmail.com>
2025-07-15 10:54:24 +0800

    fix: dispatch argument

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from network.udp_handler import UDPHandler
 from parser.message_parser import parse_message
 from utils.logger import Logger
 from core.peer import PeerManager
+from core.message_dispatcher import dispatch
 
 if __name__ == "__main__":
     logger = Logger(verbose=True)
     peer_manager = PeerManager(logger)
-    udp = UDPHandler(logger, peer_manager)
+    udp = UDPHandler(logger, peer_manager, dispatch)
     udp.start()
