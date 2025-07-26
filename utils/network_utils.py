47b6a7480938bfa05ed0ec5971f6d19f86682895
GrypheNDravize <ratoge1816@gmail.com>
2025-07-26 13:43:05 +0800

    changed

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -10,19 +10,26 @@ def send_message(msg_dict, addr, udp_socket=None):
     msg_text = craft_message(msg_dict)
     udp_socket.sendto(msg_text.encode('utf-8'), addr)
 
+# def get_local_ip():
+#     # This tries to connect to an external host, but doesn't actually send data,
+#     # just to get the outbound interface IP address.
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     try:
+#         # This IP is arbitrary; 8.8.8.8 is Google DNS
+#         s.connect(('8.8.8.8', 80))
+#         ip = s.getsockname()[0]
+#     except Exception:
+#         ip = '127.0.0.1'
+#     finally:
+#         s.close()
+#     return ip
+
 def get_local_ip():
-    # This tries to connect to an external host, but doesn't actually send data,
-    # just to get the outbound interface IP address.
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # This IP is arbitrary; 8.8.8.8 is Google DNS
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-    except Exception:
-        ip = '127.0.0.1'
-    finally:
-        s.close()
-    return ip
+    for iface, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET and not addr.address.startswith("127."):
+                return addr.address
+    return '127.0.0.1'
 
 # # Get the broadcast address for the local network.
 # def get_broadcast_address():
