6736772972ce55ca918db5f2d36fce91c44e9acf
GrypheNDravize <ratoge1816@gmail.com>
2025-08-01 20:15:24 +0800

    ping-periodically; profile-whenever set

--- core/broadcaster.py
+++ core/broadcaster.py
@@ -5,16 +5,21 @@ from utils.network_utils import send_message
 import json
 from parser.message_parser import craft_message
 
-def broadcast_profile_periodically(logger, peer_manager, interval=300): # set the interval to 20 seconds for testing
+def broadcast_profile_periodically(logger, peer_manager, interval=10): # set the interval to 20 seconds for testing
     def broadcast_loop():
         while True:
             profile = peer_manager.get_own_profile()
+            user_id = profile.get("USER_ID")
+            ping = {
+            	"TYPE": "PING",
+                "USER_ID": user_id
+			}
             # Only broadcast if the profile is complete (has USER_ID)
             if profile and profile.get("USER_ID"):
-                send_message(profile, (config.BROADCAST_ADDR, config.PORT))
+                send_message(ping, (config.BROADCAST_ADDR, config.PORT))
                 
                 #lsnp_text = craft_message(profile)
-                logger.log_send("PROFILE", f"{config.BROADCAST_ADDR}:{config.PORT}", profile)
+                logger.log_send("PING", f"{config.BROADCAST_ADDR}:{config.PORT}", ping)
             # else:
             #     logger.log("PROFILE", "Own profile not set; skipping broadcast.")
             time.sleep(interval)
