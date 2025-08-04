4ee49bc4bb276c8b338afec982bbee1b1b81abe8
Your Name <you@example.com>
2025-08-04 16:27:02 +0800

    change port number

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from utils.network_utils import get_broadcast_address
 from utils.network_utils import get_manual_broadcast
 
-PORT = 51000
+PORT = 50999
 BROADCAST_ADDR = get_manual_broadcast()
 VERBOSE = True
 TTL = 3600 # default is 3600
